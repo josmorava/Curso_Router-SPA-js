db93fe7988240ca3ebfb7ad38095520bfd6d9453
josmorava <josemorales-1234@hotmail.es>
2020-11-02 04:21:20 -0400

    Agregando archivo de rutas

---
+++ routes.js
@@ -0,0 +1,16 @@
+const routes = [
+  {
+    path: '/',
+    template: <h1>Hola</h1>
+  },
+  {
+    path: '/contacto',
+    template: <h1>Contacto</h1>
+  },
+  {
+    path: '/aboutme',
+    template: <h1>About Me</h1>
+  },
+];
+//array con las rutas home, contact, about me.
+//Que hace refencia directa a los elementos creados en index.html
